874a48665e6d36ba13989bc392b6a2da79608500
Admir Alicic <admir.alicic@gmail.com>
2018-03-18 08:56:59 +0100

    Add conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,79 @@
-"use strict";
+'use strict';
 
 console.log("App.js is running");
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of the computer',
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var user = {
+    name: 'Andrew',
+    age: 26,
+    location: 'Philadelphia'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
